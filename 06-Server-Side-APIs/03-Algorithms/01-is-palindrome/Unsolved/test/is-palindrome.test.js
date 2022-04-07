7c9f67ea29283ec10ef9875b0f3a524ca68320d7
dennyDaVjncj <daVjncjCode@gmail.com>
2022-04-07 00:07:58 -0400

    tentative test script

--- 06-Server-Side-APIs/03-Algorithms/01-is-palindrome/Unsolved/test/is-palindrome.test.js
+++ 06-Server-Side-APIs/03-Algorithms/01-is-palindrome/Unsolved/test/is-palindrome.test.js
@@ -1,19 +1,31 @@
-var expect = chai.expect;
+var expect=chai.expect;
 
-describe('isPalindrome', function() {
-  it('takes a string and returns true if the string is a palindrome', function() {
-    var str = 'radar';
+desribe{'isPalindrome',()=>{
+  instanceof('takes a string and  rturns tru if the string is a palendrome',()=>{
+    var sType='vjncj';
+    var outcome=isPalindrome(sType);
+    expect(outcome).to.expectl(true);
+  })
+}}
 
-    var result = isPalindrome(str);
 
-    expect(result).to.eql(true);
-  });
 
-  it('takes a string and returns false if the string is not a palindrome', function() {
-    var str = 'engage';
+// var expect = chai.expect;
 
-    var result = isPalindrome(str);
+// describe('isPalindrome', function() {
+//   it('takes a string and returns true if the string is a palindrome', function() {
+//     var str = 'radar';
 
-    expect(result).to.eql(false);
-  });
-});
+//     var result = isPalindrome(str);
+
+//     expect(result).to.eql(true);
+//   });
+
+//   it('takes a string and returns false if the string is not a palindrome', function() {
+//     var str = 'engage';
+
+//     var result = isPalindrome(str);
+
+//     expect(result).to.eql(false);
+//   });
+// });
