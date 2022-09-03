867db0138df42d67fc52960ef22851112e1c0e37
dennyDaVjncj <daVjncjCode@gmail.com>
2022-09-02 20:42:05 -0400

    version control

--- 05-Third-Party-APIs/05-Module-Project/main/dVc/UX.js
+++ 05-Third-Party-APIs/05-Module-Project/main/dVc/UX.js
@@ -1,6 +1,15 @@
 var activity={}//why isn't this an array?
 var bulletPoints=document.querySelector()
 
-var actvtyAlgrthm=()=>{
-    var actnItm=document.
+var actvtyAlgrthm=(taskText,taskDate,taskList)=>{//param values get passed in & executed upon
+    var actnItm=document.querySelector('listItem').addClass('someClass');
+    var contentSpan=document.querySelector('targetSpanElmnt');
+        .addClass('someAttrbte')
+        .text(taskDate);
+    var taskBoxModel=document.querySelector('pElmnt')
+        .addClass('someClass')
+        .text(taskText);
+
+    taskList.append(contentSpan,taskBoxModel);
+    auditTask(taskList);
 }
